1faa90ea839dcbe8a60836b77f16694d4ce5f2cb
viswax2000 <viswax2000@gmail.com>
2017-05-19 00:15:17 +0530

    asd

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -17,8 +17,8 @@ String password =  "postgresql";				//System.getenv("PGPASSWORD");
 Connection connection = DriverManager.getConnection(databaseURL, username,
 password);
 if (connection != null) {
-String SQL = "select a.string AS first, b.string AS second, c.string AS noun
-from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1";
+String SQL = "select a.string AS first, b.string AS second, c.string AS noun " + 
+			  "from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1";
 Statement stmt = connection.createStatement();
 ResultSet rs = stmt.executeQuery(SQL);
 while (rs.next()) {
